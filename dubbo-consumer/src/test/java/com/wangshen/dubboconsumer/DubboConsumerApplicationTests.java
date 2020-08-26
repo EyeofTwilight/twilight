25e170df4130c05aefbd828c3e94ceacf40cac26
wangshen <wangshen>
2020-08-26 22:32:24 +0800

    打包报错修改

--- dubbo-consumer/src/test/java/com/wangshen/dubboconsumer/DubboConsumerApplicationTests.java
+++ dubbo-consumer/src/test/java/com/wangshen/dubboconsumer/DubboConsumerApplicationTests.java
@@ -1,20 +1,9 @@
 package com.wangshen.dubboconsumer;
 
-import com.wangshen.dubboconsumer.service.impl.AnnotationAction;
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.annotation.Resource;
 
 @SpringBootTest
 class DubboConsumerApplicationTests
 {
-	@Resource
-	private AnnotationAction annotationAction;
 
-	@Test
-	void contextLoads()
-	{
-		System.out.println(annotationAction.doSayHello("wangshen"));
-	}
 }
